242d2f61c68c60967a94ea6de723dfc8eb644288
Demián <demiansamirtorres@gmail.com>
2021-01-22 23:05:27 -0300

    first cleaning

---
+++ src/CardsApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CardsApp = () => {
+  return (
+    <div>
+      <h1>CardsApp</h1>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CardsApp } from './CardsApp'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <CardsApp />,
   document.getElementById('root')
 );
 
